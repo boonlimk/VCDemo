a39cdfbf79981f47825ebc9050408c48c14c6823
boonlimk <32057681+boonlimk@users.noreply.github.com>
2020-02-06 16:37:49 +0800

    Refresh Base

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -1,23 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 public class MoveScript : MonoBehaviour
 {
-    float value;
-    Vector2 movePos;
-
-    [Range(1.0f,30.0f)]
-    public float speed = 1.0f;
-
-    [Range(1.0f, 10.0f)]
-    public float ringSize = 1.0f;
-
-    public bool VerticalMove = true;
-    public bool HorizontalMove = false;
-    public bool clockwiseMove = true;
-
+    float amount;
+    public float height = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,33 +15,7 @@ public class MoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (clockwiseMove)
-        {
-            value -= Time.deltaTime;
-        }
-        else
-        {
-            value += Time.deltaTime;
-        }
-
-        if(VerticalMove)
-        {
-            movePos.y = Mathf.Sin(value * speed);
-        }
-        else
-        {
-            movePos.y = 0.0f;
-        }
-
-        if (HorizontalMove)
-        {
-            movePos.x = Mathf.Cos(value * speed);
-        }
-        else
-        {
-            movePos.x = 0.0f;
-        }
-
-        transform.position = new Vector3(movePos.x,movePos.y, 0.0f) * ringSize;
+        transform.position = Vector3.up * (Mathf.Sin(amount) * height);
+        amount += Time.deltaTime;
     }
 }
