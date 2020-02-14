9030d13430de174790170e11cc349261089ff881
junchoo <59987152+junchoo@users.noreply.github.com>
2020-02-14 12:23:47 +0800

    Add files via upload

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,32 +4,34 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    Transform selfTr;
-    public float moveSpeed = 0.1f;
-    public float jumpHeight = 5f;
-    bool isJumping;
-
-    void Update()
+    float amount;
+    public float height = 1.0f;
+    public float moveSpeed = 3f;
+    // Start is called before the first frame update
+    void Start()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)) // left arrow key
-        {
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow)) // right arrow key
-        {
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow) && !isJumping) // up arrow key
-        {
-            isJumping = true;
-            transform.Translate(Vector3.up * moveSpeed * jumpHeight * Time.deltaTime);
-        }
+        
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    // Update is called once per frame
+    void Update()
     {
-        isJumping = false;
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+            amount += Time.deltaTime;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Up") * moveSpeed);
+        }
+
+            
     }
 }
