e85097658f0ea6d260581646c468d18f25dea706
Magthylius <magthylius@gmaillcom>
2020-02-14 12:11:04 +0800

    Committed enemy script
    - Added enemy movement (left right arrow keys)
    - Added enemy jump

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    Transform selfTr;
+    public float moveSpeed = 0.1f;
+    public float jumpHeight = 5f;
+    bool isJumping;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) // left arrow key
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) // right arrow key
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) && !isJumping) // up arrow key
+        {
+            isJumping = true;
+            transform.Translate(Vector3.up * moveSpeed * jumpHeight * Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+}
