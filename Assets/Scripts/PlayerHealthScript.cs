987dd8acbee588825d4f87922bc4394d97533b9e
bxkachii <rayna.tang20@gmail.com>
2020-06-09 17:16:16 +0800

    Player Health
    Health minus but sprite not working

---
+++ Assets/Scripts/PlayerHealthScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthScript : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int curHealth;
+
+    public HealthBarScript healthbar;
+    void Start()
+    {
+        curHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            Damage(10);
+        }
+    }
+
+    void Damage (int damage)
+    {
+        curHealth -= damage;
+        healthbar.SetHealth(curHealth);
+    }
+}
