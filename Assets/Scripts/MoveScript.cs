c7837b07d442f11fa0e4ef1e631bfc4aa346390b
EleenCYW <65888313+EleenCYW@users.noreply.github.com>
2020-06-09 16:40:20 +0800

    DoubleJumpAdded

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -7,6 +7,7 @@ public class MoveScript : MonoBehaviour
     public float jumpHeight = 10.0f;
     private Rigidbody2D rb;
     public float moveSpeed = 5.0f;
+    private bool canDoubleJump = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +32,12 @@ public class MoveScript : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             transform.position += Vector3.up * Time.deltaTime * jumpHeight;
+            canDoubleJump = true;
+            if (canDoubleJump && Input.GetButtonDown("Jump"))
+            {
+                transform.position += Vector3.up * Time.deltaTime * jumpHeight;
+                canDoubleJump = false;
+            }
         }
     }
 }
