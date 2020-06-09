786c5f6cd1abdf6899e1da1b632cf1d0bf4b89b2
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2020-06-09 16:27:34 +0800

    OnlyCommitAssets

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Audio;
+using UnityEngine;
+//Welcome to some leak of my GPS1 Codes, Not full but part of it. Hopefully Mr.Boon can give some idea about my codes
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds; //Store Every Sounds, Refer Inspector
+
+    public static AudioManager instance;
+    void Awake()
+    {
+        if (instance == null) //For Multiple Scene Purpose avoid audiomanger missing
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("BGM"); //Play BGM
+    }
+
+    public void Play(string name) //Play Sound source
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) //Warning Debug
+        {
+            Debug.LogWarning("Sounds: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Pause(string name) //Pause Sound source
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) //Warning Debug
+        {
+            Debug.LogWarning("Sounds: " + name + " not found!");
+            return;
+        }
+        s.source.Pause();
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound //Custom Sound Class
+{
+    public string name; //Name for the audio
+
+    public AudioClip clip; //Audio file
+
+
+    [Range(0f, 1f)]
+    public float volume; //control volume
+    [Range(.1f, 3f)]
+    public float pitch; //pitch of your audio file
+
+    public bool loop; //loop music only
+
+    [HideInInspector]
+    public AudioSource source;
+}
