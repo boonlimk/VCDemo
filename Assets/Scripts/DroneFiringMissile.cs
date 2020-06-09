bb93c0de53ceb05acd125286c780fa3586b2742f
Leemeoyi <50307529+Leemeoyi@users.noreply.github.com>
2020-06-09 16:12:57 +0800

    Added Homing Missle and drone firing script

---
+++ Assets/Scripts/DroneFiringMissile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneFiringMissile : MonoBehaviour
+{
+    public float timer;
+    private float countdownTimer;
+    public GameObject missile;
+
+    private void Start()
+    {
+        countdownTimer = timer;
+    }
+
+    private void Update()
+    {
+        countdownTimer -= Time.deltaTime;
+        if (countdownTimer <= 0f)
+        {
+            Instantiate(missile, transform.position, Quaternion.identity);
+            countdownTimer = timer;
+        }
+    }
+}
--- Assets/Scripts/HomingMissileScript.cs
+++ Assets/Scripts/HomingMissileScript.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class HomingMissileScript : MonoBehaviour
 {
+    public float speed = 7.0f;
+    public float rotationSpeed = 10.0f;
+    
+    public Transform targetPlayer;
 
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 dir = (Vector2)targetPlayer.position - rb.position;
+        dir.Normalize();
+
+        float rotationAmount = Vector3.Cross(dir, transform.up).z;
+
+        rb.angularVelocity = -rotationAmount * rotationSpeed;
+        rb.velocity = transform.up * speed;
+    }
 }
