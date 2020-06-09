7aa884fb3a98e0570bfc262b9d3b866054f0daa4
JellyFeng <yapjianfeng1998@gmail.com>
2020-06-09 16:37:38 +0800

    Sod
    z

---
+++ Assets/Bulletshoot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletshoot : MonoBehaviour
+{
+    public GameObject bullet;
+    bool hasShot = false;
+    public float shootDelay = 0.5f;
+    float shootTimer = 0.0f;
+
+    public float bulletSpeed = 5.0f;
+    public float DestoryDelay = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasShot == false)
+        {
+            
+            shootBounce bulletMove = Instantiate(bullet, transform.position, Quaternion.identity).GetComponent<shootBounce>();
+            if (bulletMove != null)
+            {
+                bulletMove.Initialize(Vector3.up, bulletSpeed, DestoryDelay);
+            }
+            hasShot = true;
+        }
+    
+        if (hasShot == true)
+        {
+            if (shootTimer >= shootDelay)
+            {
+                hasShot = false;
+                shootTimer = 0.0f;
+            }
+            else
+            {
+                shootTimer += Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ Assets/shootBounce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootBounce : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float DestroyDelay = 5.0f;
+    public Vector3 moveVector = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Initialize(Vector3 dir, float speed, float delay)
+    {
+        moveVector = dir;
+        moveSpeed = speed;
+        DestroyDelay = delay;
+        Destroy(this.gameObject, DestroyDelay);
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+    }
+}
